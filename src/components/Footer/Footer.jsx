525464c53f9d9a6327376618ffbbe68ffed6c2aa
Dafne <lire.alarcon.06@gmail.com>
2026-06-27 21:40:39 -0600

    Feat: Create footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,13 @@
+import github from "../../images/github.png"
+import github from "../../images/facebook.png"
 export default function Footer(){
     return(
-        <></>
+        <footer>
+            <p className="footer__copyright">© 2026 Supersite, Powered by News API</p>
+            <p className="footer__text">Home</p>
+            <p className="footer__text">Practicum by Dafne Pineda</p>
+            <img className="footer__media" src="{github}"/>
+            <img className="footer__media" src="{facebook}"/>
+        </footer>
     )
 }
