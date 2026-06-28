c32600b466375beb7b8609b2b985ec20eeb494ba
Dafne <lire.alarcon.06@gmail.com>
2026-06-28 09:23:24 -0600

    Style: Add style for nothing found form

--- src/components/Main/components/NewsCardList/NothingFound/NothingFound.jsx
+++ src/components/Main/components/NewsCardList/NothingFound/NothingFound.jsx
@@ -1,12 +1,9 @@
-import notFound from "../../../../../images/notFound.png"
-
 export default function NothingFound(){
     return(
         <div className="nothing-found">
-            <img className="nothing-found__image" src={notFound}/>
+            <div className="nothing-found__image"/>
             <h2 className="nothing-found__title">Nothing found</h2>
-            <p className="nothing-found__text">Sorry, but nothing matched 
-your search terms.</p>
+            <p className="nothing-found__text">Sorry, but nothing matched your search terms.</p>
         </div>
     )
 }
