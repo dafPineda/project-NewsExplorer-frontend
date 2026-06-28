0c9ca142d506aed616a852c040464b1b4b3d6d2a
Dafne <lire.alarcon.06@gmail.com>
2026-06-28 11:15:31 -0600

    Style: Add style for card list form

--- src/components/Main/components/NewsCardList/NewsCard/NewsCard.jsx
+++ src/components/Main/components/NewsCardList/NewsCard/NewsCard.jsx
@@ -1,13 +1,13 @@
-export default function NewsCard(){
+export default function NewsCard({link, date, title, text, edit}){
     return(
             <li className="card">
                 <div className="card__flag"/>
-               <img className="card__image"/>
+               <img className="card__image" src={link}/>
                 <div className="card__info">
-                    <p className="card__info-date"></p>
-                    <h2 className="card__info-title"></h2>
-                    <p className="card__info-text"></p>
-                    <p className="card__info-edit"></p>
+                    <p className="card__info-date">{date}</p>
+                    <h2 className="card__info-title">{title}</h2>
+                    <p className="card__info-text">{text}</p>
+                    <p className="card__info-edit">{edit}</p>
                 </div>
             </li>
     )
--- src/components/Main/components/NewsCardList/NewsCardList.jsx
+++ src/components/Main/components/NewsCardList/NewsCardList.jsx
@@ -5,12 +5,32 @@ import NewsCard from "./NewsCard/NewsCard";
 export default function NewsCardList(){
     return(
         <section className="new-card-list">
-            {/* <NothingFound/> */}
-            {/* <Preloader/> */}
+            <NothingFound/>
+            <Preloader/>
             <div className="new-card-list__result">
                 <h1 className="result__title">Search result</h1>
                 <ul className="cards">
-                    <NewsCard/>
+                    <NewsCard 
+                    link="https://rarehistoricalphotos.com/wp-content/uploads/2023/12/historical-colorized-photos.jpg"
+                    date="November 4, 2020" 
+                    title="Everyone Needs a Special 'Sit Spot' in Nature"
+                    text= "We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.из местных чудес природы."
+                    edit="national geographic"
+                    />
+                    <NewsCard 
+                    link="https://rarehistoricalphotos.com/wp-content/uploads/2023/12/historical-colorized-photos.jpg"
+                    date="November 4, 2020" 
+                    title="Everyone Needs a Special 'Sit Spot' in Nature"
+                    text= "We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.из местных чудес природы."
+                    edit="national geographic"
+                    />
+                    <NewsCard 
+                    link="https://rarehistoricalphotos.com/wp-content/uploads/2023/12/historical-colorized-photos.jpg"
+                    date="November 4, 2020" 
+                    title="Everyone Needs a Special 'Sit Spot' in Nature"
+                    text= "We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.из местных чудес природы."
+                    edit="national geographic"
+                    />
                 </ul>
                 <button className="result-button">Show more</button>
             </div>
