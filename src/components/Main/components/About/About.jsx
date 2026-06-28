4e3320b52a4f4c2a983b113cd0c26e70a221c8c4
Dafne <lire.alarcon.06@gmail.com>
2026-06-28 00:17:20 -0600

    Feat: Give design to component about

--- src/components/Main/components/About/About.jsx
+++ src/components/Main/components/About/About.jsx
@@ -2,14 +2,14 @@ export default function About(){
     return(
         <section className="about" id="about">
             <h2 className="about__title">About the author</h2>
-            <div className="about image"/>
+            <div className="about__image"/>
             <p className="about__text">
 I am Dafne from Mexico. I am a Programming Technician and a future Software Engineer. 
 Some of the technologies I work with are JavaScript, Java, Python, SQL, and React, among others. 
 I really enjoy Frontend development, although I feel more confident working on the Backend while 
 I continue learning and improving my skills.
             </p>
-            <p className="about__text">
+            <p className="about__text about__text-down">
                 During my time as a student in the TripleTen Bootcamp, I have learned and practiced modern web development
  technologies through different projects. The instructors and classmates who supported me throughout this 
  journey have been very kind and patient. I feel grateful to have reached this point, where I can 
